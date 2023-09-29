04ce06b66acd368109bfb5f6c32d0c4e3fe047f4
millywanderi <wanderinyambura5@gmail.com>
2023-09-29 22:11:18 +0300

    api

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Create instance of Flask"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from flask import make_response
+from flask import jsonify
+#from flask_cors import CORS
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_session(db):
+    """This method closes the session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    hst = os.getenv("HBNB_API_HOST", default="0.0.0.0")
+    prt = int(os.getenv("HBNB_API_PORT", default=5000))
+    app.run(host=hst, port=prt, threaded=True)
