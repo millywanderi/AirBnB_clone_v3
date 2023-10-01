e081dd6052d09f7a841294baac52c139b0e9c801
reggyshicky <reginahshikanda@gmail.com>
2023-10-01 14:27:12 +0300

    hashing

--- models/user.py
+++ models/user.py
@@ -26,4 +26,9 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if 'password' in kwargs:
+            password = kwargs['password']
+            m = hashlib.md5()
+            m.update(str.encode(password))
+            kwargs['password'] = m.hexdigest()
         super().__init__(*args, **kwargs)
